7d8af6545a9abc47cc2d2c0b10d48182cadb4b9a
CSS313 <singh.chetan.313@gmail.com>
2018-03-07 01:57:53 +0530

    Addedd articleOne data object and a function to create html template and also the lines to respond to corresponding request

--- server.js
+++ server.js
@@ -5,21 +5,67 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article-One | Chetan Singh',
+    heading: 'Article one',
+    date: 'Feb 26, 2018',
+    content: `
+        <p>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio eu feugiat pretium nibh ipsum consequat nisl vel. Massa eget egestas purus viverra accumsan in nisl nisi. Cursus euismod quis viverra nibh. Sit amet volutpat consequat mauris nunc congue nisi vitae suscipit. Pretium vulputate sapien nec sagittis. Neque vitae tempus quam pellentesque nec nam. Tortor posuere ac ut consequat semper. Massa tincidunt dui ut ornare lectus. Eros donec ac odio tempor orci dapibus ultrices in iaculis. Elementum nibh tellus molestie nunc non. 
+        </p>`
+};
+
+function createTemplate(data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>${title}</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+    
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/> 
+                <h3>${heading}
+                </h3>
+                <div>${date}
+                </div>
+                <div>
+                ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+`;
+    return htmlTemplate;
+    }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-})
+    res.send(createTemplate(articleOne));
+});
 
 app.get('/article-two', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-})
+});
 
 app.get('/article-three', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-})
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
