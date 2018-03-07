8b12797d38bc330a4b8d72d561e43a52c4a8a651
CSS313 <singh.chetan.313@gmail.com>
2018-03-07 21:03:31 +0530

    Added code to create XMLHttp request object and then make & respond to request

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,25 @@
-console.log('Loaded!');
+//Counter Code
+var button = document.getElementById('ctr');
 
-var element = document.getElementById('main-text');
-element.innerHTML = 'CSS313' ;
+button.onclick = function () {
 
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-};
-
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
+    // Create a request object.
+    var request = new XMLHttpRequest();
+    // Capture the response and store it in a variable.
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // Take some action.
+            if (request.status === 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+            }
+        }
+        // Not done yet.
+        
+    };
+    // Make the request.
+    request.open('GET', 'http://singhchetan313.imad.hasura-app.io/counter' , true);
+    request.send(null);
+    
 };
